ab79228ee6e0633640de0ffe9a790241b06729f5
mpanizza <marco.panizza@gmail.com>
2022-05-08 11:05:08 +0200

    generate resources: users, roles (REST API)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, RolesModule],
   controllers: [AppController],
   providers: [AppService],
 })
