28e7c59065cca423d93d70ab574fa7fbe64168a0
mpanizza <marco.panizza@gmail.com>
2022-05-09 11:38:06 +0200

    rescource decorator

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -1,1 +1,23 @@
-export class Role {}
+import { 
+    Entity, 
+    PrimaryGeneratedColumn, 
+    Column,
+    JoinColumn,
+    OneToMany
+ } from "typeorm"
+ 
+ import { User } from "../../users/entities/user.entity"
+ 
+ @Entity()
+export class Role {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+
+    @OneToMany(() => User, (user) => user.role) 
+    users: User[]
+
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { 
+    Entity, 
+    PrimaryGeneratedColumn, 
+    Column,
+    ManyToOne
+ } from "typeorm"
+ 
+ import { Role } from "../../roles/entities/role.entity"
+ 
+ @Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    firstName: string
+
+    @Column()
+    lastName: string
+	
+    @Column({
+       nullable: true
+    })
+    eMail: string
+
+    @ManyToOne(() => Role, (role) => role.users)
+    role: Role
+
+
+}
