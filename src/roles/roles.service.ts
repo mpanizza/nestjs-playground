709b5ff4d0c7dde176fafa0d47395c61f3b21e07
mpanizza <marco.panizza@gmail.com>
2022-05-20 10:14:30 +0200

    all services methods promise and return objects

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateRoleDto } from './dto/create-role.dto';
@@ -13,38 +13,50 @@ export class RolesService {
     @InjectRepository(User) public readonly userRepository: Repository<User>,
   ) {}
   
-  async create(createRoleDto: CreateRoleDto) {
+  async create(createRoleDto: CreateRoleDto): Promise<Role> {
     const role = this.repository.create(createRoleDto);
     console.log("Role DTO " + JSON.stringify(createRoleDto) + " | " +JSON.stringify(role) );
     await this.repository.save(role);
-    return 'This action adds a new role' + JSON.stringify(createRoleDto);
+    return role;
   }
 
-  async findAll() {
+  async findAll(): Promise<Role[]> {
     const roles = await this.repository.find();
     console.log("Loaded roles: " + JSON.stringify(roles) )
     return roles;
   }
 
-  async findOne(id: number) {
+  async findOne(id: number): Promise<Role> {
     const role = await this.repository.findOne(id,  {
       relations: ['users']
     });
+
+    if (!role) {
+      throw new NotFoundException(`Role ${id} not found`);
+    }
+
     console.log("Role " + id + " is: " + JSON.stringify(role) )
     return role;
   }
 
-  async update(id: number, updateRoleDto: UpdateRoleDto) {
+  async update(id: number, updateRoleDto: UpdateRoleDto): Promise<Role>  {
     const role =  await this.repository.findOne(id);
     console.log("DTO  " + JSON.stringify(updateRoleDto));
     role.name = (updateRoleDto.name == undefined) ? role.name : updateRoleDto.name
     console.log(`name ${updateRoleDto.name}`);
-    await this.repository.save(role);
-    return `This action updates a #${id} role`;
+    return this.repository.save(role);
   }
 
-  async remove(id: number) {
+  async remove(id: number): Promise<Role> {
+    const deletedRole =  await this.repository.findOne(id, {
+      relations: ['users']
+    });
+
+    if (!deletedRole) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+    
     await this.repository.delete(id);
-    return `This action removes a #${id} role`;
+    return deletedRole;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -14,7 +14,7 @@ export class UsersService {
     @InjectRepository(Role) public readonly roleRepository: Repository<User>,
   ) { }
   
-  async create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto): Promise<User> {
     const roleReq = await this.roleRepository.findOne(createUserDto.roleId,  {
       relations: ['users']
     })
@@ -24,27 +24,37 @@ export class UsersService {
     console.log( "User DTO : "+ (createUserDto instanceof CreateUserDto) + " = " + JSON.stringify(createUserDto));
     console.log( "User object : "+ (user instanceof User) + " = " + JSON.stringify(user));
     console.log( "requested role : "+ (roleReq instanceof Role) + " = " + JSON.stringify(roleReq));
-    return 'This action adds a new user' + JSON.stringify(createUserDto);
+    return user;
   }
 
-  async findAll() {
+  async findAll(): Promise <User[]> {
     const users = await this.repository.find();
     console.log("Loaded users: " + JSON.stringify(users) )
     return users;
   }
 
-  async findOne(id: number) {
+  async findOne(id: number): Promise <User> {
     const user =  await this.repository.findOne(id, {
       relations: ['role']
     });
+
+    if (!user) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+
     console.log("User " + id + " is: " + JSON.stringify(user) )
     return user;
   }
 
-  async update(id: number, updateUserDto: UpdateUserDto) {
+  async update(id: number, updateUserDto: UpdateUserDto): Promise <User> {
     const user =  await this.repository.findOne(id, {
       relations: ['role']
     }); 
+
+    if (!user) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+
     console.log("DTO  " + JSON.stringify(updateUserDto));
     user.firstName = (updateUserDto.firstName == undefined) ? user.firstName : updateUserDto.firstName
     console.log(`firstName ${updateUserDto.firstName}`);
@@ -53,12 +63,19 @@ export class UsersService {
     user.eMail = (updateUserDto.eMail == undefined) ? user.eMail : updateUserDto.eMail
     console.log(`eMail ${updateUserDto.eMail}`);
     console.log(`roleId ${updateUserDto.roleId}`);
-    await this.repository.save(user);
-    return `This action updates a #${id} user`;
+    return this.repository.save(user);
   }
 
-  async remove(id: number) {
+  async remove(id: number): Promise<User> {
+    const deletedUser =  await this.repository.findOne(id, {
+      relations: ['role']
+    });
+
+    if (!deletedUser) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+    
     await this.repository.delete(id);
-    return `This action removes a #${id} user`;
+    return deletedUser;
   }
 }
